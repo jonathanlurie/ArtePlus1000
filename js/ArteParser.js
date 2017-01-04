75cd04038e486e1c532c814c6f45ce1e3e5fac0d
Jonathan Lurie <lurie.jo@gmail.com>
2017-01-04 11:00:48 -0500

    done with the lib

--- js/ArteParser.js
+++ js/ArteParser.js
@@ -6,9 +6,13 @@ var ArteParser = function( programURL ){
   this._programURL = programURL;
   this._programID = null;
   this._programLang = null;
+  this._rawData = null;
   this._programData = null;
   this._onLoadedCallback = null;
-
+  this._onErrorCallback = null;
+  this._onSuggestionsFetchedCallback = null;
+  this._suggestions = [];
+  this._numberOfSuggestion = 0;
   this._extractProgramID();
 }
 
@@ -37,13 +41,21 @@ ArteParser.prototype.fetchData = function(){
     var jsonInfoURL = "https://api.arte.tv/api/player/v1/config/" +
       this._programLang + "/" + this._programID;
 
-      getJSON(jsonInfoURL, function(data) {
-        that._parseJsonObject(data);
-      }, function(status) {
-      	console.log('Something went wrong.');
-      });
+    getJSON(jsonInfoURL, function(data) {
+      that._parseJsonObject(data);
+    }, function(status) {
+    	console.log('Something went wrong.');
+    });
 
   }
+}
+
+
+/**
+* @return the URL of the TV program
+*/
+ArteParser.prototype.getProgramURL = function(){
+  this._programURL
 }
 
 
@@ -54,14 +66,22 @@ ArteParser.prototype.fetchData = function(){
 * @param {Object} jObj - TV program related JSON file parsed as an object.
 */
 ArteParser.prototype._parseJsonObject = function(jObj){
+
   var that = this;
 
-  // TODO: test validity
-  // TODO: recuperer la collection
-  // TODO: recuperer les reco (postroll)
+  // If the program URL is wrong, then we trigger the error callback and end.
+  if("custom_msg" in jObj.videoJsonPlayer){
+    if(this._onErrorCallback){
+      this._onErrorCallback( jObj.videoJsonPlayer.custom_msg.msg );
+    }
+    return;
+  }
+
+  this._rawData = jObj;
 
   // Fetching basic info
   this._programData = {
+    url: this._programURL,
     title: jObj.videoJsonPlayer.VTI,
     cover: jObj.videoJsonPlayer.VTU.IUR,
     duration: jObj.videoJsonPlayer.videoDurationSeconds,
@@ -104,4 +124,69 @@ ArteParser.prototype._parseJsonObject = function(jObj){
 */
 ArteParser.prototype.onLoaded = function( cb ){
   this._onLoadedCallback = cb;
+}
+
+
+/**
+* Defines the callback when the data fetching failled.
+* @param {function} cb - Callback. Called with one arg: the fail message
+*/
+ArteParser.prototype.onError = function( cb ){
+  this._onErrorCallback = cb;
+}
+
+
+/**
+* Defines a callback when all the data from the suggestions are fetched.
+* @param {function} cb - callback. Called with one arg: the array of suggestions.
+*/
+ArteParser.prototype.onSuggestionsFetched = function( cb ){
+  this._onSuggestionsFetchedCallback = cb;
+}
+
+
+/**
+* Once fetchData is done, the suggestions for this TV program can be fetched too.
+* Be sure to call this within the onLoaded() callback, otherwise, the data won't be ready.
+* When all the suggestions are fetched, the onSuggestionsFetched() callback is called.
+*/
+ArteParser.prototype.fetchSuggestions = function(){
+  if(!this._rawData){
+    console.error("The method fetchData must be called first and the data must be valid. It's actually safer to call this method from the onLoaded() callback.");
+    return;
+  }
+
+  var that = this;
+
+  var postrollURL = this._rawData.videoJsonPlayer.postroll;
+  //console.log(postrollURL);
+
+  getJSON(postrollURL, function(data) {
+    that._numberOfSuggestion =  data.videoList.length;
+
+    // for each suggestion, we try to fetch it.
+    data.videoList.forEach( function(vid){
+      var suggestionProgramURL = vid.VUP;
+
+      // we build an ArteParser for each suggestion too
+      var sugg = new ArteParser(suggestionProgramURL);
+      sugg.fetchData();
+
+      // Much inception in this code!
+      sugg.onLoaded( function(data){
+        that._suggestions.push(data);
+
+        if(that._suggestions.length == that._numberOfSuggestion){
+          if(that._onSuggestionsFetchedCallback){
+            that._onSuggestionsFetchedCallback(that._suggestions);
+          }
+        }
+      });
+
+    })
+
+  }, function(status) {
+    console.log('Something went wrong with the suggestions.');
+  });
+
 }
--- readme.md
+++ readme.md
@@ -1,20 +1,51 @@
-Arte:
+# What?
+Arte+1000JS is just a small browser side library, there is nothing graphic in it. Though, you can use it for your own fancy web project coupled with monsters like React/Angular and all the modern things they do these days.
 
-URL programme:
-http://www.arte.tv/guide/fr/062863-000-A/charlie-louise
+Get the data from [Arte+7](http://www.arte.tv/guide/fr/plus7) TV programs. This contains:
+- the title (text)
+- the original URL on Arte+7 (text)
+- the video information in several versions (depending on language and quality), with for each:
+  - the URL to the MP4 file (text)
+  - the bitrate (integer)
+  - the width (integer)
+  - the height (integer)
+  - a label containing language, dubbing and possibly subtitle information (text)
+- a cover image (url to jpg)
+- a long abstract (text)
+- a short abstract (text)
+- the duration in second (integer)
+- a list of suggestions to other Arte+7 programs, each containing the same info as mentioned above.
 
---> ID: 062863-000-A
 
-URL JSON du fichier:
-https://api.arte.tv/api/player/v1/config/fr/062863-000-A
+# How?
+See the the relatively short `index.html`, but it's something like that:
+
+```js
+// create an instance of ArteParser
+var ap = new ArteParser("http://www.arte.tv/guide/fr/062863-000-A/charlie-louise");
+ap.fetchData();
 
 
-Format:
-https://api.arte.tv/api/player/v1/config/LANG/PROGRAM_ID
+// called when the TV program data are all fetched
+ap.onLoaded( function(programData){
+  console.log(programData);
 
-avec LANG: fr ou de a gerer si different
+  // Optional: Fetching the suggestions can be done only when the data for _this_ program are fetched
+  ap.fetchSuggestions();
+});
 
-reco:
-https://api.arte.tv/api/player/v1/recommendedPostroll/fr/062863-000-A?platform=ARTEPLUS7
+// An error may occur, i.e. if the program url does not lead to any program
+ap.onError( function(msg){
+  console.log(msg);
+});
 
-https://api.arte.tv/api/player/v1/recommendedPostroll/fr/062863-000-A?platform=ARTEPLUS7
+// Since we called ap.fetchSuggestions() earlier, we have to deal with the result, here an array
+ap.onSuggestionsFetched( function(programDataArray){
+  console.log(programDataArray);
+})
+```
+
+Note: If you open `index.html` in a web browser, everything is displayed in console! (nothing in the page)
+
+# License
+MIT
